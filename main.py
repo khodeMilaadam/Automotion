fc8649d305466f4e73a84f77dbad4d77e5fd0ba6
Miladitor <128414239+Miladitor@users.noreply.github.com>
2024-10-08 10:18:57 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome()
+
+
+driver.get("https://www.google.com/")
+
